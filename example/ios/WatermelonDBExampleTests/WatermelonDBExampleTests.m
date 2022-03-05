9f3cf6f3c5cd9a89247105e0c6d0b1249f7361bb
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2022-03-05 23:10:59 +0100

    chore(example): rename WatermelonDB example app

--- example/ios/WatermelonDBExampleTests/WatermelonDBExampleTests.m
+++ example/ios/WatermelonDBExampleTests/WatermelonDBExampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FlipperDatabasesExampleTests : XCTestCase
+@interface WatermelonDBExampleTests : XCTestCase
 
 @end
 
-@implementation FlipperDatabasesExampleTests
+@implementation WatermelonDBExampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
