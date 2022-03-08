11a42383d418baa633f163b80e5d995975de6015
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2022-03-08 23:17:45 +0100

    chore: update PouchDB example app to include driver setup

--- example3/src/infrastructure/database/index.ts
+++ example3/src/infrastructure/database/index.ts
@@ -5,6 +5,22 @@ const db = new PouchDB('db', {
   adapter: 'react-native-sqlite',
 });
 
+/// ReactNativeFlipperDatabases - START
+
+if (__DEV__) {
+  // Import connectDatabases function and required DBDrivers
+  const {
+    connectDatabases,
+    PouchDB: PouchDBDriver,
+  } = require('react-native-flipper-databases');
+
+  connectDatabases([
+    new PouchDBDriver([db]), // Pass in database definitions
+  ]);
+}
+
+/// ReactNativeFlipperDatabases - END
+
 export async function loadTasksFromDB(): Promise<Task[]> {
   const res: PouchDBResults<Task> = await db.allDocs({
     include_docs: true,
