91c84cf8aea2eb9a66c793f571a91c1edb83e177
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2022-03-14 22:13:20 +0100

    chore(example4): update Vasern example app to include driver setup

--- example4/src/infrastructure/database/index.tsx
+++ example4/src/infrastructure/database/index.tsx
@@ -6,3 +6,19 @@ export const VasernDB = new Vasern({
   schemas: [TaskSchema],
   version: 1,
 });
+
+/// ReactNativeFlipperDatabases - START
+
+if (__DEV__) {
+  // Import connectDatabases function and required DBDrivers
+  const {
+    connectDatabases,
+    VasernDB: VasernDBDriver,
+  } = require('react-native-flipper-databases');
+
+  connectDatabases([
+    new VasernDBDriver(VasernDB), // Pass in database definitions
+  ]);
+}
+
+/// ReactNativeFlipperDatabases - END
